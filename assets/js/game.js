85c4ac5ca3a916fe020adc2221de92a5c7b90ab9
joel57 <67278919+joel57@users.noreply.github.com>
2022-07-17 11:06:04 -0700

    added the if , else section

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // you can also log muiltiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -40,12 +41,33 @@ var fight = function() {
 
     //check players health
     if (playerHealth <= 0) {
-      
-    }
-  
+      window.alert(playerName + " has lost!");
+    } else {
+      window.alert(playerName + " still has " + playerHealth + " health left."); 
+    
 
-  
-};
+    //if player chose to skip.
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+      //confirm player wants to skip.
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+      //if yes (true), leave fight
+      if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        //subtract money from playerMoney for skipping.
+        playerMoney = playerMoney - 2;
+      }
+
+      //if no (false) ask question again by running fight() again
+      else{
+        fight();
+      }
+
+    window.alert(playerName + "has chosen to skip the fight!");
+  } else {
+    window.alert ("You need to choose a valid option. Try again!");
+  }
+}
 
 // execute function
 fight();
